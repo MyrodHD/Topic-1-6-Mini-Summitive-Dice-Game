6a751eb0b9f699326a1c9a14ebbdfe3efe0b4ee2
MyrodHD <Krysalink@outlook.com>
2026-02-23 13:18:47 -0500

    Add project files.

---
+++ Topic 1-6 Mini Summitive Dice Game/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Topic_1_6_Mini_Summitive_Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool done = false;
+
+            int userBet;
+            int userBank = 100;
+            string userChoice;
+
+            while (!done)
+            {
+                Console.Write("How much would you like to bet: ");
+                while (!Int32.TryParse(Console.ReadLine(), out userBet) || userBet < 0 || userBet > userBet) 
+                {
+                    Console.Write("Invalid bet. Please enter another bet:");
+                    Console.ReadLine();
+                }
+
+                Console.WriteLine("What do you think the roll will be?");
+                Console.WriteLine("------------------");
+                Console.WriteLine("Double     Not Double");
+                Console.WriteLine();
+                Console.WriteLine("Even Sum   Odd Sum ");
+                Console.WriteLine("------------------");
+                Console.Write("Your choice: ");
+                userChoice = Console.ReadLine().ToLower().Trim();
+                
+            }
+        }
+    }
+}
